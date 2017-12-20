86f035164877825d73972b40c5d3962f58e72616
Taras <vakulkataras@gmail.com>
2017-12-20 06:54:39 +0200

    Fix bag

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -5,7 +5,7 @@ class Partners extends React.Component {
     render() {
         return(
             <div>
-                <section id="partners-header" style={{backgroundImage: 'url("./src/images/partners.png")'}}>
+                <section id="partners-header" style={{backgroundImage: `url('./src/images/partners.png')`}}>
                     <div className="container">
                         <div className="row align-items-center fill-viewport">
                             <div className="col-lg-12 text-center">
